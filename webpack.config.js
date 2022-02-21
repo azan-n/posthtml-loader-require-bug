734127a239a82ca0242c4c92cbffd12ad32d1689
Azan <azan.farooqi@cryptlex.com>
2022-02-21 13:05:32 +0530

    feat: add html-loader bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const posthtml = require('posthtml');
 
 module.exports = {
     mode: 'production',
@@ -14,19 +15,37 @@ module.exports = {
         rules: [
             {
                 test: /\.html$/i,
-                use: [
-                    "html-loader",
-                    {
-                        loader: 'posthtml-loader',
-                        // options: {
-                        //     plugins: function () {
-                        //         return [
-                        //             require('posthtml-highlight')({ inline: true })
-                        //         ]
-                        //     }
-                        // }
+                // With PostHTML Loader Config inside this file
+                // use: [
+                //     "html-loader",
+                //     {
+                //         loader: 'posthtml-loader',
+                //         // options: {
+                //         //     plugins: function () {
+                //         //         return [
+                //         //             require('posthtml-highlight')({ inline: true })
+                //         //         ]
+                //         //     }
+                //         // }
+                //     }
+                // ],
+
+                // With PostHTML used in html-loader preprocessor option
+
+                loader: "html-loader",
+                options: {
+                    preprocessor: (content, loaderContext) => {
+                        posthtml([
+                            require('posthtml-highlight')({ inline: true }),
+                        ])
+                            .process(content, { sync: true })
+                            .then((result) => { return result.html })
+                            .catch((error) => {
+                                loaderContext.emitError(error);
+                                return content;
+                            })
                     }
-                ],
+                },
             },
         ],
     }
